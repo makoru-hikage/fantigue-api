39256d83d626800694b76380987057a79a36ac4a
unknown <kurt.ma.coll@gmail.com>
2019-08-24 04:29:48 +0800

    Enable connection to the DB

--- index.js
+++ index.js
@@ -6,7 +6,32 @@ const routes = require('./routes');
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const Sequelize = require('sequelize')
 
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_TYPE,
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        define: {
+            underscored: true
+        }
+    }
+)
+
+//Check if this is connected well to the database
+sequelize
+    .authenticate()
+    .then(() => console.log(`DB successfully connected to ${process.env.DB_HOST} at port: ${process.env.DB_PORT}!`))
+
+//Initiate Routes
 app.use('/', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
